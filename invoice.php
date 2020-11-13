302f3a15f1b52e2b5da3632dd0e7511f2d3dd441
mustakim79 <mustakimkrs@gmail.com>
2020-11-13 17:32:51 +0530

    hide print button when printing

--- invoice.php
+++ invoice.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Invoice</title>
     <?php include 'bfile.html'; ?>
+    <link href="css3/print.css" rel="stylesheet" media="print">
 </head>
 
 <body>
