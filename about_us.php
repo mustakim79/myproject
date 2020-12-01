8130835af2f7df0c218a02b6179f984cd7d0f7cd
mustakim79 <mustakimkrs@gmail.com>
2020-12-01 16:45:56 +0530

    added some detail in about us page

--- about_us.php
+++ about_us.php
@@ -4,10 +4,37 @@ include 'header.php';
 ?>
 <main>
     <div class="container">
-        <div class="text-center text-capitalize h-75 py-5"
-            style="display:flex;align-items:center;justify-content:center;">
-            <div class="">
-                <h1>We are making food for you</h1>
+        <h1 class="text-center my-3"><b>About Us</b></h1>
+        <div class="card-deck my-3">
+            <div class="card shadow">
+                <img src="photos/chef1.jpg" style="object-fit:cover;    height: 210px;object-position: top;"
+                    class="card-img-top" alt="..." style="height:150px;">
+                <div class="card-body text-center">
+                    <h5>Chef</h5><br>
+                    <b>
+                        <h3>Edward Robinson</h3>
+                    </b>
+                </div>
+            </div>
+            <div class="card shadow">
+                <img src="photos/chef woman.jpg" style="object-fit:cover;height: 210px;object-position: top;"
+                    class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5>Chef</h5><br>
+                    <b>
+                        <h3>Amanda Stark</h3>
+                    </b>
+                </div>
+            </div>
+            <div class="card shadow">
+                <img src="photos/chef3.jpg" style="object-fit:cover;    height: 210px;object-position: top;" alt="..."
+                    class="card-img-top">
+                <div class="card-body text-center">
+                    <h5>Chef</h5><br>
+                    <b>
+                        <h3>Ryan Ricks</h3>
+                    </b>
+                </div>
             </div>
         </div>
     </div>
